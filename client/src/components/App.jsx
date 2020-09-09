ec3fe5f2df873b8537acec493d7abe72bd743766
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-09-09 12:05:02 -0500

    Sets up basic server/client  boilerplate

---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`MVP is hanging out at http://localhost:${port}`));
